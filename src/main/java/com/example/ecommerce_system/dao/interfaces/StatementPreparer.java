873306d6a29dcd300220f8cd0355d43696b41ab6
amalipalal <palal.asare@amalitech.com>
2026-02-13 21:50:59 +0000

    chore: delete store and dao classes

--- src/main/java/com/example/ecommerce_system/dao/interfaces/StatementPreparer.java
+++
@@ -1,9 +0,0 @@
-package com.example.ecommerce_system.dao.interfaces;
-
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-@FunctionalInterface
-public interface StatementPreparer {
-    void prepare(PreparedStatement ps) throws SQLException;
-}
--- src/main/java/com/example/ecommerce_system/util/SqlAndParams.java
+++
@@ -1,5 +0,0 @@
-package com.example.ecommerce_system.util;
-
-import java.util.List;
-
-public record SqlAndParams(String sql, List<Object> params) {}
