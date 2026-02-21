21770799d955d09e4e19e110131d0b771ab304d9
amalipalal <palal.asare@amalitech.com>
2026-02-21 18:08:42 +0000

    feat: add custom OAuth2User to hold user details

---
+++ src/main/java/com/example/ecommerce_system/config/OAuth2UserWithUserObject.java
@@ -0,0 +1,33 @@
+package com.example.ecommerce_system.config;
+
+import com.example.ecommerce_system.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Map;
+
+@AllArgsConstructor
+@Getter
+public class OAuth2UserWithUserObject implements OAuth2User {
+
+    private final OAuth2User oAuth2User;
+    private final User user;
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return oAuth2User.getAttributes();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return oAuth2User.getAuthorities();
+    }
+
+    @Override
+    public String getName() {
+        return oAuth2User.getName();
+    }
+}
