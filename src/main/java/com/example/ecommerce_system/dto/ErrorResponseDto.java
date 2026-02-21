fcf6adc678149e0310355fe067130e3ea436941f
amalipalal <palal.asare@amalitech.com>
2026-02-21 07:10:18 +0000

    refactor: add non-null inclusion to generic dtos
    - SuccessResponseDto
    - ErrorResponseDto

--- src/main/java/com/example/ecommerce_system/dto/ErrorResponseDto.java
+++ src/main/java/com/example/ecommerce_system/dto/ErrorResponseDto.java
@@ -1,5 +1,7 @@
 package com.example.ecommerce_system.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import org.springframework.http.HttpStatus;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record ErrorResponseDto<T> (HttpStatus status, String message, T error){}
